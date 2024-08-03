6bbc9a645ab8eec7aa40954ce5abc8f029e55796
MrCrisisPotato <supersacredkiller@gmail.com>
2024-08-03 16:20:29 +0530

    Basic Design Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Octokit } from '@octokit/rest';
 import './App.css';
+
+
+octokit.request("GET /");
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <navbar> <h1> Github Profile </h1> 
+      <div>
+        <div className="link">Contact us
+          <div className="dropdown">
+            <p><a href="#contact">Sourav S K</a></p>
+            <p>Aravind Suresh</p>
+          </div>
+        </div>
+        <div className="link">About us
+          <div className="dropdown">
+            <p>Sourav S K</p>
+            <p>Aravind Suresh</p>
+          </div>
+        </div>
+      </div>
+      </navbar>
     </div>
+    <h3 id="contact">Contact</h3>
+    </>
   );
 }
 
